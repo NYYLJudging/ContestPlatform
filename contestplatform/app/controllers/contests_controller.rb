2dcfd87a3f55d7e06ef7e19de4274ef3fc8b3805
Patrick Canny <patrickcanny@ku.edu>
2017-10-21 11:07:20 -0500

    Adding Index for Contests

--- contestplatform/app/controllers/contests_controller.rb
+++ contestplatform/app/controllers/contests_controller.rb
@@ -1,4 +1,12 @@
 class ContestsController < ApplicationController
+  def index
+    @contests = Contest.all
+  end
+
+  def show
+    @contest = Contest.find(params[:id])
+  end
+
   def new
   end
 
